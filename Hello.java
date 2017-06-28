e26f0e492180f4b5a65d38a759f08e1c8bdd1627
Jinbc <wink1235@naver.com>
2017-06-28 17:43:52 +0900

    added output "Bitkeeper"

--- Hello.java
+++ Hello.java
@@ -2,7 +2,8 @@ public class Hello{
 	public static void main (String[] args){
 		System.out.println("Hello git!");
 		System.out.println("what is Git?");
-		System.out.println("That's DVCS?");
+		System.out.println("hey");
+		
 	}
 }
 
